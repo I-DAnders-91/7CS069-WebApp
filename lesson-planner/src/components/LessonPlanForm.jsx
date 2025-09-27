5a7cc8b6722dc9958189414e54ab830c0777bcfe
I-DAnders-91 <d.anderson5@wlv.ac.uk>
2025-09-27 22:01:50 +0000

    Add event handlers to lesson plan form

--- lesson-planner/src/components/LessonPlanForm.jsx
+++ lesson-planner/src/components/LessonPlanForm.jsx
@@ -1,16 +1,42 @@
+import { useState } from "react";
+
 function LessonPlanForm() {
+
+    const [values, setValues] = useState({
+        lessonObjective: '',
+        successCriteria: '',
+        subject: '',
+        ncStatement: '',
+        lessonPlan: '',
+        resources: null,
+        lessonLink: ''
+    })
+
+    const handleChanges = (e) => {
+        setValues({...values, [e.target.name]: e.target.value})
+    }
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        console.log('Form submitted with values:', values);
+    }
+
     return (
         <div className="form">
             <h1>Create a New Lesson...</h1>
-            <form>
+            <form onSubmit={handleSubmit}>
                 <label htmlFor="lessonObjective" required>Lesson Objective</label>
-                <input type="text" placeholder="Enter Lesson Objective" />
+                <input type="text" name="lessonObjective" placeholder="Enter Lesson Objective" 
+                onChange={(e) => handleChanges(e)}/>
 
                 <label htmlFor="successCriteria">Success Criteria</label>
-                <input type="text" placeholder="Enter Success Criteria" />
+                <input type="text" name="successCriteria" placeholder="Enter Success Criteria" 
+                onChange={(e) => handleChanges(e)}/>
 
                 <label htmlFor="subject" required>Subject</label>
-                <select name="subject" id="subject">
+                <select name="subject" id="subject"
+                onChange={(e) => handleChanges(e)}>
+                    <option value="" disabled selected>Select a subject</option>
                     <option value="maths">Maths</option>
                     <option value="english">English</option>
                     <option value="science">Science</option>
@@ -27,18 +53,22 @@ function LessonPlanForm() {
                 </select>
 
                 <label htmlFor="ncStatement">National Curriculum Statement</label>
-                <select name="ncStatement" id="ncStatement">
-                    <option value="placeholder">Coming Soon</option>
+                <select name="ncStatement" id="ncStatement"
+                onChange={(e) => handleChanges(e)}>
+                    <option value="placeholder" disabled selected>Coming Soon</option>
                 </select>
 
                 <label htmlFor="lessonPlan" required>Lesson Plan</label>
-                <textarea name="text" id="lessonPlan" cols="30" rows="10" placeholder="Type your lesson plan here" />
+                <textarea name="lessonPlan" cols="30" rows="10" placeholder="Type your lesson plan here" 
+                onChange={(e) => handleChanges(e)}/>
 
                 <label htmlFor="resources">Resources</label>
-                <input type="file" placeholder="Upload Lesson Files" name="resources" />
+                <input type="file" placeholder="Upload Lesson Files" name="resources" 
+                onChange={(e) => handleChanges(e)}/>
 
                 <label htmlFor="lessonLink">Useful Links</label>
-                <input type="url" placeholder="Copy useful URLs" name="lessonLink" />
+                <input type="url" placeholder="Copy useful URLs" name="lessonLink" 
+                onChange={(e) => handleChanges(e)}/>
 
                 <button type="submit">Create Lesson</button>
                 <button type="button">Delete</button>
