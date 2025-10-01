11e17982d4f2a54d3169c22e97ef07c25ad5dd81
I-DAnders-91 <d.anderson5@wlv.ac.uk>
2025-10-01 19:17:52 +0000

    Refactor Lesson plan form
    Add responsive functionality using Bootstrap

--- lesson-planner/src/components/LessonPlanForm.jsx
+++ lesson-planner/src/components/LessonPlanForm.jsx
@@ -25,54 +25,78 @@ function LessonPlanForm() {
     return (
         <div className="form">
             <h1>Create a New Lesson...</h1>
-            <form onSubmit={handleSubmit}>
-                <label htmlFor="lessonObjective" required>Lesson Objective</label>
-                <input type="text" name="lessonObjective" placeholder="Enter Lesson Objective" 
-                onChange={(e) => handleChanges(e)}/>
+            <form onSubmit={handleSubmit} className="row g-3">
 
-                <label htmlFor="successCriteria">Success Criteria</label>
-                <input type="text" name="successCriteria" placeholder="Enter Success Criteria" 
-                onChange={(e) => handleChanges(e)}/>
+                <div className="col-md-8">
+                    <label htmlFor="lessonObjective" className="form-label" required>Lesson Objective</label>
+                    <input type="text" id="lessonObjective" className="form-control" placeholder="Enter Lesson Objective" 
+                    onChange={(e) => handleChanges(e)}/>
+                </div>
+                <div className="col-md-4">
+                    <label htmlFor="subject" className="form-label" required>Subject</label>
+                    <select id="subject" className="form-select"
+                    onChange={(e) => handleChanges(e)}>
+                        <option value="" disabled selected>Select subject</option>
+                        <option value="maths">Maths</option>
+                        <option value="english">English</option>
+                        <option value="science">Science</option>
+                        <option value="geography">Geography</option>
+                        <option value="history">History</option>
+                        <option value="languages">Languages</option>
+                        <option value="art">Art</option>
+                        <option value="music">Music</option>
+                        <option value="dt">DT</option>
+                        <option value="computing">Computing</option>
+                        <option value="re">RE</option>
+                        <option value="pshe">PSHE</option>
+                        <option value="pe">PE</option>
+                    </select>
+                </div>
 
-                <label htmlFor="subject" required>Subject</label>
-                <select name="subject" id="subject"
-                onChange={(e) => handleChanges(e)}>
-                    <option value="" disabled selected>Select a subject</option>
-                    <option value="maths">Maths</option>
-                    <option value="english">English</option>
-                    <option value="science">Science</option>
-                    <option value="geography">Geography</option>
-                    <option value="history">History</option>
-                    <option value="languages">Languages</option>
-                    <option value="art">Art</option>
-                    <option value="music">Music</option>
-                    <option value="dt">Design Technology</option>
-                    <option value="computing">Computing</option>
-                    <option value="re">Religious Education</option>
-                    <option value="pshe">Personal, Social, Health, and Economics</option>
-                    <option value="pe">Physical Education</option>
-                </select>
+                <div className="col-md-8">
+                    <label htmlFor="successCriteria" class="form-label">Success Criteria</label>
+                    <input type="text" id="successCriteria" class="form-control" placeholder="Enter Success Criteria" 
+                    onChange={(e) => handleChanges(e)}/>
+                </div>
+                <div className="col-md-4">
+                    <label htmlFor="ncStatement" className="form-label">NC Statement</label>
+                    <select id="ncStatement" className="form-select"
+                    onChange={(e) => handleChanges(e)}>
+                        <option value="placeholder" disabled selected>Coming Soon</option>
+                    </select>
+                </div>
 
-                <label htmlFor="ncStatement">National Curriculum Statement</label>
-                <select name="ncStatement" id="ncStatement"
-                onChange={(e) => handleChanges(e)}>
-                    <option value="placeholder" disabled selected>Coming Soon</option>
-                </select>
+                <div className="col-md-12">
+                    <label htmlFor="lessonPlan" className="form-label" required>Lesson Plan</label>
+                    <textarea id="lessonPlan" className="form-control" rows="10" placeholder="Type your lesson plan here" 
+                    onChange={(e) => handleChanges(e)}/>
+                </div>
 
-                <label htmlFor="lessonPlan" required>Lesson Plan</label>
-                <textarea name="lessonPlan" cols="30" rows="10" placeholder="Type your lesson plan here" 
-                onChange={(e) => handleChanges(e)}/>
+                <div className="col-md-6">
+                    <label htmlFor="resources" className="form-label">Resources</label>
+                    <input type="file" className="form-control" placeholder="Upload Lesson Files" id="resources" 
+                    onChange={(e) => handleChanges(e)}/>
+                </div>
 
-                <label htmlFor="resources">Resources</label>
-                <input type="file" placeholder="Upload Lesson Files" name="resources" 
-                onChange={(e) => handleChanges(e)}/>
+                <div className="col-md-6">
+                    <label htmlFor="lessonLink">Useful Links</label>
+                    <div className="input-group">
+                        <button className="btn btn-outline-secondary" type="button" id="addUrl">Add</button>
+                        <input type="url" className="form-control" placeholder="Copy useful URLs" id="lessonLink" 
+                        onChange={(e) => handleChanges(e)}/>
+                        <ul>
+                        //TODO: Links should appear here when added
+                            <li>Useful URL 1 </li>
+                        </ul>
+                        </div>
 
-                <label htmlFor="lessonLink">Useful Links</label>
-                <input type="url" placeholder="Copy useful URLs" name="lessonLink" 
-                onChange={(e) => handleChanges(e)}/>
-
-                <button type="submit">Create Lesson</button>
-                <button type="button">Delete</button>
+                </div>
+                <div className="col-md-8">
+                    <button type="submit" className="btn btn-primary">Create Lesson</button>
+                    </div>
+                <div className="col-md-4">
+                    <button type="button" id="delete-btn">Delete</button>
+                    </div>
             </form>
         </div>
     )
